08efc093d094bca387058c8b34b4366087a33203
CodeBotOfficial <codebotoff@outlook.fr>
2026-05-10 10:25:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.GuildMember],
+});
+
+// Chargement du module principal
+require('./rc-community')(client);
+
+// Connexion du bot
+client.login(process.env.DISCORD_TOKEN);
